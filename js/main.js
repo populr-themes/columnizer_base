4b9264d8d70d016e5772d692204096adc4f4fed4
John Horstman <github@johnhorstman.com>
2013-04-15 13:48:55 -0500

    Remove columnizer asset height equalizer JS; update styling to match branded theme skeleton

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-var fixColumnHeights = function() {
-  $('.columnizer-row').each(function(){
-    var maxHeight = 0;
-    $(this).find('.asset-inner').each(function(){
-      $(this).height('auto');
-      var innerHeight = $(this).innerHeight();
-      if (innerHeight > maxHeight) {
-        maxHeight = innerHeight;
-      }
-    });
-    $(this).find('.asset-inner').innerHeight(maxHeight);
-  });
-}
+// var fixColumnHeights = function() {
+//   $('.columnizer-row').each(function(){
+//     var maxHeight = 0;
+//     $(this).find('.asset-inner').each(function(){
+//       $(this).height('auto');
+//       var innerHeight = $(this).innerHeight();
+//       if (innerHeight > maxHeight) {
+//         maxHeight = innerHeight;
+//       }
+//     });
+//     $(this).find('.asset-inner').innerHeight(maxHeight);
+//   });
+// }
 
-$(document).on('pop-initialized', function(){
-  $('.columnizer-row .asset').live('initialize', fixColumnHeights);
-  $('.columnizer-row .asset').live('destroy', function(){
-    $(this).find('.asset-inner').height('auto');
-  });
-});
+// $(document).on('pop-initialized', function(){
+//   $('.columnizer-row .asset').live('initialize', fixColumnHeights);
+//   $('.columnizer-row .asset').live('destroy', function(){
+//     $(this).find('.asset-inner').height('auto');
+//   });
+// });
