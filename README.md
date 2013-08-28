4573ae5234cebba0f3bf2dd0276cf18026b3eb5b
John Horstman <github@johnhorstman.com>
2013-08-28 15:11:58 -0500

    Numerous updates to simplify & improve the base theme before John's departure
    * Added & improved comments
    * Simplified & organized the CSS
    * Added a custom grid (to educate new themers on how it's done)
    * Simplified & improved responsive behavior

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 columnizer_base
 ===================
 
-Simple columnizer theme to use as a starting point
+This repo contains a simple Populr theme. It works as a great starting point for building a theme of your own. The repo contains the minimum amount of code necessary to work as a functional theme on its own, while still including enough help to train you on the ins and outs of the theming system.
+
+## Theming documentation
+
+For full documentation on theme design & development in Populr, visit [our developer center](http://developers.populr.me/).
+
+## About Populr
+
+Populr is a tool that lets you easily create and share simple web pages that grab attention, close deals, and deliver information.
+
+[Populr.me](http://populr.me)
--- js/main.js
+++ js/main.js
@@ -1,20 +1,9 @@
-// var fixColumnHeights = function() {
-//   $('.columnizer-row').each(function(){
-//     var maxHeight = 0;
-//     $(this).find('.asset-inner').each(function(){
-//       $(this).height('auto');
-//       var innerHeight = $(this).innerHeight();
-//       if (innerHeight > maxHeight) {
-//         maxHeight = innerHeight;
-//       }
-//     });
-//     $(this).find('.asset-inner').innerHeight(maxHeight);
-//   });
-// }
+/************************************************************************
+// For extensive details about creating a Populr theme, please visit
+// http://developers.populr.me/theming
+*/
+
 
 // $(document).on('pop-initialized', function(){
-//   $('.columnizer-row .asset').live('initialize', fixColumnHeights);
-//   $('.columnizer-row .asset').live('destroy', function(){
-//     $(this).find('.asset-inner').height('auto');
-//   });
+//
 // });
